c7f57edab1281a8fe6bab54e6ca1d9bdcb1f58f8
cptharris <67209324+cptharris@users.noreply.github.com>
2023-09-07 13:29:41 -0700

    Initial Commit

---
+++ Emoji-Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji-Art
+//
+//  Created by Caleb Harris on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
